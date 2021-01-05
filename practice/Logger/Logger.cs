adab1d91468a16f1834db3b4d67f2ef914df83a4
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-05 20:39:01 +0200

    Created logger.cs

---
+++ practice/Logger/Logger.cs
@@ -0,0 +1,27 @@
+﻿namespace practice.Logger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Logger
+    {
+        private static readonly Lazy<Logger> LazyLog = new Lazy<Logger>(() => new Logger());
+
+        public static Logger GetInstance()
+        {
+            return LazyLog.Value;
+        }
+
+        public void LogAsync(string message, Exception ex = null)
+        {
+            if (ex != null)
+            {
+                message = $"{message}{ex.Message}";
+            }
+
+            string log = $"{DateTime.UtcNow.ToString("hh:mm:ss")}:{message}";
+            Console.WriteLine(log);
+        }
+    }
+}
