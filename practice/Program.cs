9d5afa1060a0a7f519e773b4c404b37e32a0c5b3
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-05 19:55:00 +0200

    Added StyleCop

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace practice
+﻿namespace practice
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
