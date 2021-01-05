836fca00ee072cdc868d20e05868c540f9b0ecf3
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-05 20:24:13 +0200

    Added List's methods

--- practice/Collection/MyColletion.cs
+++ practice/Collection/MyColletion.cs
@@ -2,20 +2,37 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
     using System.Threading.Tasks;
 
     public class MyColletion<T>
     {
         private List<T> list = new List<T>();
+        private Random rnd = new Random();
 
         public async void AddAsync(T item)
         {
-            Random rnd = new Random();
             await Task.Run(() =>
             {
-                Task.Delay(rnd.Next(0, 400) + 100);
+                Task.Delay(this.rnd.Next(0, 400) + 100);
                 this.list.Add(item);
+            });
+        }
+
+        public async void AddRangeAsync(T[] item)
+        {
+            await Task.Run(() =>
+            {
+                Task.Delay(this.rnd.Next(0, 400) + 100);
+                this.list.AddRange(item);
+            });
+        }
+
+        public async void RemoveItemAsync(T item)
+        {
+            await Task.Run(() =>
+            {
+                Task.Delay(this.rnd.Next(0, 400) + 100);
+                this.list.Remove(item);
             });
         }
     }
