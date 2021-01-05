8802ad6945614cb24017fdd0e5748cbbcfcf8f77
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-05 20:19:09 +0200

    Added new method

--- practice/Collection/MyColletion.cs
+++ practice/Collection/MyColletion.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace practice.Collection
+﻿namespace practice.Collection
 {
-    class MyColletion
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MyColletion<T>
     {
+        private List<T> list = new List<T>();
+
+        public async void AddAsync(T item)
+        {
+            Random rnd = new Random();
+            await Task.Run(() =>
+            {
+                Task.Delay(rnd.Next(0, 400) + 100);
+                this.list.Add(item);
+            });
+        }
     }
 }
